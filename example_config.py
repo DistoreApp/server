14e1e1e4b0653be6aa0db70f8cc9c2430d037d34
Liam H <35095843+ir-3@users.noreply.github.com>
2019-12-11 09:40:47 -0600

    Update example_config.py

--- example_config.py
+++ example_config.py
@@ -1,3 +1,18 @@
+"""
+!IMPORTANT!
+
+This is the EXAMPLE config and WILL HAVE TO be 
+renamed to config.py and edited correctly to work.
+
+You can change which config to use based on how you
+want the webserver to run, I think its pretty self
+explanatory. You can also add new configs that change
+different settings for the flask app. 
+
+You can find a list of those here: 
+https://flask.palletsprojects.com/en/1.1.x/config/#builtin-configuration-values
+"""
+
 class Config(object):
     TESTING = False
     DEBUG = False
